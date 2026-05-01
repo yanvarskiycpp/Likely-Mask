edff91f755bc0de7d03e42829a2f3592c0ba0811
январский <dolboebovaruk@gmail.com>
2026-05-01 17:18:19 +0200

    Update build.gradle.kts

--- app/core/build.gradle.kts
+++ app/core/build.gradle.kts
@@ -17,10 +17,10 @@ wire {
 }
 
 android {
-    namespace = "com.topjohnwu.magisk.core"
+    namespace = "com.yanvarsky.likelymask.core"
 
     defaultConfig {
-        buildConfigField("String", "APP_PACKAGE_NAME", "\"com.topjohnwu.magisk\"")
+        buildConfigField("String", "APP_PACKAGE_NAME", "\"com.yanvarsky.likelymask\"")
         buildConfigField("int", "APP_VERSION_CODE", "${Config.versionCode}")
         buildConfigField("String", "APP_VERSION_NAME", "\"${Config.version}\"")
         buildConfigField("int", "STUB_VERSION", Config.stubVersion)
